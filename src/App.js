ec9ee6b2fc72629ee59cd09ed32179c6ec1a2848
hyokyun <gyrbs22@gmail.com>
2021-08-21 23:40:38 +0900

    build route component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import {HashRouter, Route} from 'react-router-dom'
+
+import About from './router/About'
+import Home from './router/Home'
 
 function App() {
-  return <span>Renew Homepage!</span>
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  )
 }
 
 export default App;
