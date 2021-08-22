794afa93761ba6efcbc8debbf1b95e655b44e84f
hyokyun <gyrbs22@gmail.com>
2021-08-22 12:00:49 +0900

    Navigation by href method

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import {HashRouter, Route} from 'react-router-dom'
 
 import About from './router/About'
 import Home from './router/Home'
+import Navigation from './component/Navigation'
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
     </HashRouter>
